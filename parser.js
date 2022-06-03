466a6c0502a186ba3b6ce1fb11bbd560c3614b2d
yuniquintero <yuni.quintero@gmail.com>
2022-06-03 22:03:49 +0200

    refine outputs

--- parser.js
+++ parser.js
@@ -2,10 +2,10 @@ const HLS = require('hls-parser');
 const { Variant } = HLS.types;
 
 const urls = ["https://moctobpltc-i.akamaihd.net/hls/live/571329/eight/playlist.m3u8",
-    //"https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8",
+    "https://cph-p2p-msl.akamaized.net/hls/live/2000341/test/master.m3u8",
     "http://amssamples.streaming.mediaservices.windows.net/634cd01c-6822-4630-8444-8dd6279f94c6/CaminandesLlamaDrama4K.ism/manifest(format=m3u8-aapl)",
-    "http://amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest(format=m3u8-aapl)"]
-//"http://amssamples.streaming.mediaservices.windows.net/69fbaeba-8e92-4740-aedc-ce09ae945073/AzurePromo.ism/manifest(format=m3u8-aapl)"];
+    "http://amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest(format=m3u8-aapl)",
+    "http://amssamples.streaming.mediaservices.windows.net/69fbaeba-8e92-4740-aedc-ce09ae945073/AzurePromo.ism/manifest(format=m3u8-aapl)"];
 let variantsDict = {};
 let objectsArr = []
 let keys = ['width', 'height']
@@ -26,13 +26,14 @@ async function run() {
         let payload = await fetch(urls[idx]);
         variantsDict[idx] = await parseStreamData(payload);
     }
-    //algorithmA()
+    algorithmA()
     algorithmB()
 }
 
 run();
 
 function algorithmA() {
+    console.log("Algorithm A")
     let neededResolutions = getResolutions(variantsDict[0])
     neededResolutions.pop()
     let matchingArr = []
@@ -51,6 +52,15 @@ function algorithmA() {
         if (idx != 0) {
             let resolutionsArr = getResolutions(variantsDict[idx])
             resolutionsArr.pop()
+
+            resolutionsArr = resolutionsArr.filter( // remove duplicates
+                (s => o =>
+                    (k => !s.has(k) && s.add(k))
+                        (keys.map(k => o[k]).join('|'))
+                )
+                    (new Set)
+            );
+
             let tempMatch = []
 
             for (let x of neededResolutions) {
@@ -67,9 +77,9 @@ function algorithmA() {
 
             if (neededString === matchString) {
                 matchingArr.push(objectsArr[idx])
-                console.log("matching resolution", urls[idx], resolutionsArr)
+                console.log("Matching resolution from", urls[idx], resolutionsArr)
             } else {
-                console.log("not matching resolution", urls[idx], resolutionsArr)
+                console.log("Not matching resolution from", urls[idx], resolutionsArr)
             }
         }
     }
@@ -78,9 +88,10 @@ function algorithmA() {
 }
 
 function algorithmB() {
+    console.log("Algorithm B")
     let resolutions = []
     for (let idx in variantsDict) {
-        let tempArr = getResolutions(variantsDict[idx]) // eliminar duplicados
+        let tempArr = getResolutions(variantsDict[idx])
         tempArr.pop()
         tempArr = tempArr.filter( // remove duplicates
             (s => o =>
@@ -90,15 +101,16 @@ function algorithmB() {
                 (new Set)
         );
         resolutions.push(tempArr)
+        console.log("Resolutions from this url: ", urls[idx], tempArr)
     }
     let results = []
     let matchingArr = []
 
     for (let idx in resolutions) {
         let arr = resolutions[idx]
-        for (let obj of arr) { // contador, rescatar obj, cuantas veces aparece es igual a la cantidad de los arrays menos 1
+        for (let obj of arr) {
             let contFound = 0
-            for (let arrT of resolutions) { // var found
+            for (let arrT of resolutions) {
                 if (arr != arrT) {
                     for (let objT of arrT) {
                         if (obj.width == objT.width && obj.height == objT.height) {
@@ -109,11 +121,10 @@ function algorithmB() {
                 }
             }
             if (contFound == resolutions.length - 1) {
-                matchingArr.push(objectsArr[idx]) // idx of resolutions
+                matchingArr.push(objectsArr[idx])
                 results.push(obj)
-                console.log(urls[idx])
             }
-        } // recuperar el idx del stream y el idx de los variantes
+        }
     }
 
     results = results.filter( // remove duplicates
@@ -124,8 +135,7 @@ function algorithmB() {
             (new Set)
     );
 
-    console.log("resolutions", resolutions)
-    console.log("intersection", results)
+    console.log("Intersection", results)
 }
 
 function getResolutions(variantsArr) {
